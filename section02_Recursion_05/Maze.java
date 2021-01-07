eeccb8b7f7475c548acfbbca43ec5a20760f0c6c
dragonbead95 <dragonbead95@naver.com>
2021-01-07 17:45:45 +0900

    done:)

---
+++ section02_Recursion_05/Maze.java
@@ -0,0 +1,51 @@
+package section02_Recursion_05;
+
+public class Maze {
+	private static int N=8;
+	private static int[][] image = {
+			{1,0,0,0,0,0,0,1},
+			{0,1,1,0,0,1,0,0},
+			{1,1,0,0,1,0,1,0},
+			{0,0,0,0,0,1,0,0},
+			{0,1,0,1,0,1,0,0},
+			{0,1,0,1,0,1,0,0},
+			{1,0,0,0,1,0,0,1},
+			{0,1,1,0,0,1,1,1},
+			
+	};
+	private static final int BACKGROUND_PIXCEL = 0; // πË∞Ê «»ºø
+	private static final int IMAGE_PIXCEL = 1;	 	// ¿ÃπÃ¡ˆ «»ºø
+	private static final int VISITED_PIXCEL = 2;	// πÊπÆ«— «»ºø
+	
+	public static int countingCell(int x, int y)
+	{
+		if(x<0 || y<0 || x>=N || y>=N)
+		{
+			return 0;
+		}
+		else if(image[x][y]!=IMAGE_PIXCEL || image[x][y]==VISITED_PIXCEL)
+		{
+			return 0;
+		}
+		else 
+		{
+			image[x][y] = VISITED_PIXCEL;
+			return 1 +
+			countingCell(x-1, y) +
+			countingCell(x-1, y+1) +
+			countingCell(x, y+1) +
+			countingCell(x+1, y+1) +
+			countingCell(x+1, y) +
+			countingCell(x+1, y-1) +
+			countingCell(x, y-1) +
+			countingCell(x-1, y-1);
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(countingCell(5, 3));
+	}
+
+}
